0c20425e88013e14bfca62d2e7afc4e8310f2993
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-02 16:45:41 +0700

    Mocha chai testing for fb login done

--- server/controllers/fb.js
+++ server/controllers/fb.js
@@ -2,8 +2,9 @@ const FB = require('fb')
 
 class fbCtrl {
     static login(req,res) {
-        FB.api('me', { fields: ['id', 'name','email'], access_token: '' }, function (response) {
+        FB.api('me', { fields: ['id', 'name','email', 'picture'], access_token: '' }, function (response) {
             console.log(response)
+            console.log(response.picture.data.url)
             res.send(response)
         })
     }
