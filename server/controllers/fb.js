adbb515cdda216b92ad974c32496ab5fb3e02f6d
jasoneffendi <jasoneffendi27@gmail.com>
2017-11-03 10:14:42 +0700

    Update testing, send token using jwt

--- server/controllers/fb.js
+++ server/controllers/fb.js
@@ -1,10 +1,14 @@
 const FB = require('fb')
+const jwt = require('jsonwebtoken')
 
 class fbCtrl {
     static login(req,res) {
-        FB.api('me', { fields: ['id', 'name','email', 'picture'], access_token: '' }, function (response) {
-            console.log(response)
-            console.log(response.picture.data.url)
+        FB.api('me', { fields: ['id', 'name','email', 'picture'], access_token: req.body.accessToken }, function (response) {
+            // console.log(response)
+            // console.log(response.picture.data.url)
+            var token = jwt.sign(response, 'wild')
+            response.token = token
+            // console.log(token)
             res.send(response)
         })
     }
